62d6ae22a3743390851fd06b37e0421a0d6f9e51
MontesCodes-CodingTheFrontEnd <montiesWebDev@gmail.com>
2022-06-08 22:37:57 -0400

    updating CSS, JS, from Lenovo desk 6/8/22 #1

--- js/app.js
+++ js/app.js
@@ -15,5 +15,33 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   let timerId = setInterval(startGame, 20);
 
-  clearInterval(timerId);
+  function control(e) {
+    if (e.keyCode === 32) {
+      jump();
+    }
+  }
+
+  function jump() {
+    if (birdBottom < 500) birdBottom += 50;
+    bird.style.bottom = birdBottom + 'px';
+  }
+  document.addEventListener('keyup', jump);
+
+  function generateObstacle() {
+    let obstacleLeft = 500;
+    let randomHeight = Math.random() * 60;
+    let obstacleBottom = randomHeight;
+    const obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    gameDisplay.appendChild(obstacle);
+    obstacle.style.left = obstacleLeft + 'px';
+    obstacle.style.bottom = obstacleBottom + 'px';
+
+    function moveObstacle() {
+      obstacleLeft -= 2;
+      obstacle.style.left = obstacleLeft + 'px';
+    }
+    let timerId = setInterval(moveObstacle, 20);
+  }
+  generateObstacle();
 });
